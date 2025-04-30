72c5f94433b8808afce8a60b2e1c888b2a6cd930
Mukesh Pilaniya <pilaniyamukesh0@gmail.com>
2025-05-01 02:16:50 +0530

    Add render-markdown plugin

---
+++ nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = {
+      --'nvim-treesitter/nvim-treesitter',
+      "echasnovski/mini.icons",
+    },
+    config = function()
+      require("render-markdown").setup()
+    end,
+  }
+}
